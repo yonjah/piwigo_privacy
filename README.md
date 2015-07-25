edbe0a9d951c21e852c26a822c17e3519accc181
Yoni Jah <yonjah@gmail.com>
2015-07-25 20:19:16 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# pwigio-privacy
+# piwigio-privacy
 A small script and nginx configuration to increase the privacy of piwigo gallery
+
+This repository contains a small script that will validate users before allowing
+access to static files, so using it will actually protect your files from access by
+anyone who is not allowed to view them.
+unlike `action.php` this script will not serve the files back to the client
+and instead use nginx for serving the file and works both on the original and derivatives.
+
+_For more information check out my blog post about [Securing Private Piwigo Albums](https://ca.non.co.il/index.php/securing-private-piwigo-albums/)_
+
+## file description
+`auth.php` the auth validation script
+`piwigo-nginx-site` file contains very basic nginx site configuration with auth_request redirects to the auth.php script
+`default.png` a basic image that will be served instead of forbidden images
+
+## Install
+Since default nginx build does not enable the `auth_request` feature
+the hardest thing in the installation process will be to compile [nginx source](http://nginx.org/en/download.html)
+The actual process will be different depending on your distro and installed
+modules but here is the flags I used -
+
+```shell
+./configure \
+	--user=www-data                       \
+	--group=www-data                      \
+	--prefix=/etc/nginx                   \
+	--sbin-path=/usr/sbin/nginx           \
+	--conf-path=/etc/nginx/nginx.conf     \
+	--pid-path=/run/nginx.pid             \
+	--lock-path=/run/nginx.lock           \
+	--error-log-path=/var/log/nginx/error.log \
+	--http-log-path=/var/log/nginx/access.log \
+	--with-http_gzip_static_module        \
+	--with-http_stub_status_module        \
+	--with-http_ssl_module                \
+	--with-pcre                           \
+	--with-file-aio                       \
+	--with-http_realip_module             \
+	--without-http_scgi_module            \
+	--without-http_uwsgi_module           \
+	--with-http_auth_request_module
+```
+
+You might need to install some libs (I was missing libssl-dev for openssl and libpcre3-dev for PCRE)
+but if all goes well you can compile and install
+```shell
+make
+sudo make install
+```
+
+copy `auth.php` and  `default.png` to the root of your `piwigo` folder
+edit your nginx site file (usually under /etc/nginx/sites-available) to [auth_request](http://nginx.org/en/docs/http/ngx_http_auth_request_module.html)
+requests to /upload and derivatives file locations, you can use `piwigo-nginx-site` file as is but you'll probably want
+your actual nginx conf to be a bit more complex than that
+
+restart nginx and you should be good to go
+
+## Why this is not a plugin
+I was trying to see how to achieve the same result as a plugin but that seems a bit useless since any way
+I'll need to have a script that nginx can directly access for `auth_request` and since any way we need to change
+nginx configuration I didn't see much point in having this as a plugin.
---
+++ auth.php
@@ -0,0 +1,85 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo-Privacy - keep your piwigo content private                     |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2015 Yonijah                                             |
+// | Based on action.php file from Piwigo gallery                          |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+define('PHPWG_ROOT_PATH','./');
+session_cache_limiter('public');
+include_once(PHPWG_ROOT_PATH.'include/common.inc.php');
+
+// Check Access and exit when user status is not ok
+check_status(ACCESS_GUEST);
+
+function do_error( $code, $str ) {
+  set_status_header( $code );
+  echo $str ;
+  exit();
+}
+
+function get_file_from_path($path) {
+  $match = [];
+  \preg_match('/\d{4}\\/\d{2}\\/\d{2}\\/(\d{14}-[0-9a-f]{8})/', $path, $match);
+  return isset($match[1]) ? $match[1] : null;
+}
+
+$path = filter_input(INPUT_SERVER, 'REQUEST_URI', FILTER_SANITIZE_URL);
+$file_part = get_file_from_path($path);
+
+
+if (!$file_part) {
+  do_error(400, 'Invalid request - path');
+}
+
+$query = 'SELECT * FROM '. IMAGES_TABLE.' WHERE path LIKE \'%'.
+        pwg_db_real_escape_string($file_part).
+        '%\' LIMIT 1;';
+
+$element_info = pwg_db_fetch_assoc(pwg_query($query));
+if ( empty($element_info) ) { //make sure reply is the same for forbidden and nonexisiting files
+  do_error(401, 'Access denied');
+}
+
+// $filter['visible_categories'] and $filter['visible_images']
+// are not used because it's not necessary (filter <> restriction)
+$query='
+SELECT id
+  FROM '.CATEGORIES_TABLE.'
+    INNER JOIN '.IMAGE_CATEGORY_TABLE.' ON category_id = id
+  WHERE image_id = '.$element_info['id'].'
+'.get_sql_condition_FandF(
+  array(
+      'forbidden_categories' => 'category_id',
+      'forbidden_images' => 'image_id',
+    ),
+  '    AND'
+  ).'
+  LIMIT 1
+;';
+if ( pwg_db_num_rows(pwg_query($query))<1 ) {
+  do_error(401, 'Access denied');
+}
+
+include_once(PHPWG_ROOT_PATH.'include/functions_picture.inc.php');
+
+if ( !$user['enabled_high'] &&  strpos($element_info['path'], $path) !== false) {
+   do_error(401, 'Access denied');
+}
+
+echo 'OK';
