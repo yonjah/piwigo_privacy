1bb4fba5b14681763f7362c773e5bb5efe7f5fbb
Yoni Jah <yonjah+git@gmail.com>
2017-02-09 01:14:25 +0800

    Improved privacy using plugin features

---
+++ get.php
@@ -0,0 +1,64 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo-Privacy - keep your piwigo content private                     |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2017 Yoni jah                                             |
+// | Based on action.php file from Piwigo gallery                          |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+defined('PHPWG_ROOT_PATH') or define('PHPWG_ROOT_PATH','../../');
+session_cache_limiter('public');
+include_once(PHPWG_ROOT_PATH.'include/common.inc.php');
+include_once(PHPWG_ROOT_PATH . 'admin/include/image.class.php');
+include_once('./helper_funcs.inc.php');
+
+defined('PWG_DERIVATIVE_DIR') or define('PWG_DERIVATIVE_DIR', $conf['data_location'].'i/');
+
+global $conf;
+$conf['piwigo_privacy_debug'] = 1;
+
+if (isset($conf['piwigo_privacy_redirect_header'])) {
+	$full_path = pwg_privacy_sanitize_path(filter_input(INPUT_SERVER, 'REQUEST_URI', FILTER_SANITIZE_URL));
+	if (!$full_path) {
+		pwg_privacy_reject_access('Could not get url path');
+	}
+	list($img_id, $req_path) = explode('/', ltrim($full_path, '/'), 2);
+	$req_path = './' . $req_path;
+	if (!is_numeric($img_id) && $img_id == (int)$img_id ) {
+		pwg_privacy_reject_access("Image id '$img_id' is not numeric $full_path");
+	}
+
+} else {
+	$img_id = filter_input(INPUT_GET, 'img_id', FILTER_VALIDATE_INT);
+	$req_path = pwg_privacy_sanitize_path(filter_input(INPUT_GET, 'file', FILTER_SANITIZE_URL));
+	if (strpos($req_path, './') !== 0) {
+		$req_path = './' . ltrim($req_path, '/');
+	}
+}
+
+//reject access if no id or path
+if ( !$img_id || !$req_path ) {
+	pwg_privacy_reject_access('Could not find image id or path');
+}
+
+$path = pwg_privacy_verify_access($img_id, $req_path);
+
+if (!$path) {
+	pwg_privacy_reject_access('Access was rejected');
+}
+
+pwg_privacy_serve_file($path);
---
+++ main.inc.php
@@ -0,0 +1,67 @@
+<?php
+/*
+Version: 0.1.0
+Plugin Name: piwigo_privacy
+Plugin URI: // Here comes a link to the Piwigo extension gallery, after
+           // publication of your plugin. For auto-updates of the plugin.
+Author: Yoni Jah
+Description: Make sure only secure access is allowed to your gallery images https://github.com/yonjah/piwigo-privacy
+*/
+
+// Chech whether we are indeed included by Piwigo.
+if (!defined('PHPWG_ROOT_PATH')) die('Hacking attempt!');
+global $conf;
+
+if ($conf['derivative_url_style'] !== 1) {
+	global $page;
+	$page['errors'][] = 'Piwigo privacy requires config derivative_url_style to be set to 1 but it is set to '.$conf['derivative_url_style'].'. Plugin has been disabled';
+} else {
+	add_event_handler('get_derivative_url', 'pwg_privacy_plugin_replace_derivative_url');
+	add_event_handler('get_src_image_url', 'pwg_privacy_plugin_replace_url');
+	add_event_handler('picture_pictures_data', 'pwg_privacy_plugin_replace_picture');
+}
+
+
+function pwg_privacy_plugin_replace_picture ($picture) {
+	foreach ($picture as $key => &$image) {
+		if (isset($image['element_url'])) {
+			$image['element_url'] = pwg_privacy_modify_url($image['element_url'], $image['id']);
+		}
+		if (isset($image['src_image'])) {
+			$image['src_image']->rel_path = pwg_privacy_modify_url($image['src_image']->rel_path, $image['src_image']->id);
+		}
+	}
+	return $picture;
+}
+
+function pwg_privacy_plugin_replace_derivative_url($url, $params, $image) {
+	$url = pwg_privacy_modify_url($url, $image->id);
+	return $url;
+}
+
+function pwg_privacy_plugin_replace_url($url, $image) {
+	$url = pwg_privacy_modify_url($url, $image->id);
+	return $url;
+}
+
+
+function pwg_privacy_modify_url($url, $id) {
+	global $conf;
+	$root_url = get_root_url();
+
+	if (strpos($url, 'action.php') !== false) {
+		//were not changing action.php urls
+		return $url;
+	}
+
+	if (!isset($conf['piwigo_privacy_redirect_header'])) {
+		$image_prefix = PHPWG_PLUGINS_PATH.basename(dirname(__FILE__)).'/get.php?img_id='.$id.'&file=';
+	} else {
+		$image_prefix = $id . '/';
+	}
+	if ($root_url && strpos($url, $root_url) === 0) {
+		$url = substr($url, strlen($root_url));
+		return trim($root_url, '/') . '/' . $image_prefix . trim($url, '/');
+	}
+	return $image_prefix . trim($url, '/');
+}
